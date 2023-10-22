19df32f29bef5f8500e2067ef35191dd86130dff
Boop <onpotis@gmail.com>
2023-10-22 16:31:24 +1300

    and this is my site

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Shows and hides the menu when the menu icon is clicked
     const menuIcon = document.querySelector('.menu-icon');
     const navUl = document.querySelector('nav ul');
 
@@ -6,10 +7,13 @@ document.addEventListener('DOMContentLoaded', function() {
         navUl.classList.toggle('show');
     });
 
+    // Show and hide the header when scrolling up and down
     let lastScrollTop = 0;
     const HEADER = document.querySelector('header');
 
     window.addEventListener('scroll', function() {
+
+        // Show and hide the header when scrolling up and down
         let scrollTop = window.scrollY || document.documentElement.scrollTop;
         if (scrollTop > lastScrollTop){
             HEADER.style.top = (-HEADER.offsetHeight).toString() + "px"; // Hide header
@@ -19,7 +23,8 @@ document.addEventListener('DOMContentLoaded', function() {
             HEADER.style.transition = "top 0.5s ease-out";
         }
         lastScrollTop = scrollTop;
-
+        
+        // Change the arm images when scrolling
         var leftImg = document.querySelector('.left-image'); 
         var rightImg = document.querySelector('.right-image');
         var leftRect = leftImg.getBoundingClientRect();
@@ -36,9 +41,11 @@ document.addEventListener('DOMContentLoaded', function() {
             rightImg.src = 'images/pixel arm raising1.png';
         }
 
-        var elements = document.querySelectorAll('.i-am');
 
-        elements.forEach(function(element) {
+        // Fade in and out the "I am" elements
+        var iAmElements = document.querySelectorAll('.i-am');
+
+        iAmElements.forEach(function(element) {
             var position = element.getBoundingClientRect();
 
             // checking whether fully visible
