37d651c1990c6586a263c0c9ee7ec66eafae1ae5
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-04-24 15:22:53 +1200

    Hopefully add touch movement?

--- draggable.js
+++ draggable.js
@@ -1,13 +1,13 @@
-// Mostly taken from w3schools: https://www.w3schools.com/howto/howto_js_draggable.asp and written myself
+// Mostly taken from w3schools: https://www.w3schools.com/howto/howto_js_draggable.asp but written and modified by myself
+
 dragElement(document.getElementsByClassName("pickupable-centered"));
 
 function dragElement(elements) {
     for (let element of elements) {
-        console.log(element);
-
         var movedPositionX = 0, movedPositionY = 0, originPositionX = 0, originPositionY = 0;
 
         element.onmousedown = dragMouseDown;
+        element.ontouchstart = dragTouchStart;
 
         function dragMouseDown(event) {
             event = event || window.event;
@@ -16,10 +16,20 @@ function dragElement(elements) {
             originPositionX = event.clientX;
             originPositionY = event.clientY;
             document.onmouseup = closeDragElement;
-            document.onmousemove = elementDrag;
+            document.onmousemove = elementDragMouse;
         }
 
-        function elementDrag(event) {
+        function dragTouchStart(event) {
+            event = event || window.event;
+            event.preventDefault();
+
+            originPositionX = event.touches[0].clientX;
+            originPositionY = event.touches[0].clientY;
+            document.ontouchend = closeDragElement;
+            document.ontouchmove = elementDragTouch;
+        }
+
+        function elementDragMouse(event) {
             event = event || window.event;
             event.preventDefault();
 
@@ -39,9 +49,31 @@ function dragElement(elements) {
             }
         }
 
+        function elementDragTouch(event) {
+            event = event || window.event;
+            event.preventDefault();
+
+            movedPositionX = originPositionX - event.touches[0].clientX;
+            movedPositionY = originPositionY - event.touches[0].clientY;
+            originPositionX = event.touches[0].clientX;
+            originPositionY = event.touches[0].clientY;
+
+            element.style.left = (element.offsetLeft - movedPositionX) + "px";
+            element.style.top = (element.offsetTop - movedPositionY) + "px";
+            const rect = element.getBoundingClientRect();
+            const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+            if (rect.right + 500 > vw) {
+                element.style.right = (rect.right - vw - 110) + "px";
+            } else {
+                element.style.right = "";
+            }
+        }
+
         function closeDragElement() {
             document.onmouseup = null;
             document.onmousemove = null;
+            document.ontouchend = null;
+            document.ontouchmove = null;
         }
     }
 }
