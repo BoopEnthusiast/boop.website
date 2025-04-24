5b0f27bec76b999bec0025b7b3c15bf28fb9f66b
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2025-04-24 13:35:01 +1200

    draggable boxes

--- draggable.js
+++ draggable.js
@@ -1,3 +1,40 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Hello!");
-});
+// Mostly taken from w3schools: https://www.w3schools.com/howto/howto_js_draggable.asp and written myself
+dragElement(document.getElementsByClassName("pickupable-centered"));
+
+function dragElement(elements) {
+    for (let element of elements) {
+        console.log(element);
+
+        let movedPositionX = 0, movedPositionY = 0, originPositionX = 0, originPositionY = 0;
+
+        element.onmousedown = dragMouseDown;
+
+        function dragMouseDown(event) {
+            event = event || window.event;
+            event.preventDefault();
+
+            originPositionX = event.clientX;
+            originPositionY = event.clientY;
+            document.onmouseup = closeDragElement;
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(event) {
+            event = event || window.event;
+            event.preventDefault();
+
+            movedPositionX = originPositionX - event.clientX;
+            movedPositionY = originPositionY - event.clientY;
+            originPositionX = event.clientX;
+            originPositionY = event.clientY;
+
+            element.style.left = (element.offsetLeft - movedPositionX) + "px";
+            element.style.top = (element.offsetTop - movedPositionY) + "px";
+        }
+
+        function closeDragElement() {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
+}
